dcabcab32a0ac18218fd0f3496f87d9d9c14285b
FDL <fadelcompaore79@gmail.com>
2026-04-13 12:43:39 +0000

    feat: ajout de la pagination globale par defaut de drf dans settings

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,9 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
+    # --- Pagination globale ---
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,  # 20 résultats par page, ajustable
 }
 
 SIMPLE_JWT = {
